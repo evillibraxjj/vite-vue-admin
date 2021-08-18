f95a580005f885b97d7848729c5aa4acaed91fb5
许佳俊 <xujiajun@ihaoshenghuo.com>
2021-08-18 11:59:30 +0800

    优化代码！添加列表查询功能

--- src/api/mock/index.js
+++ src/api/mock/index.js
@@ -1,19 +1,19 @@
-import Mock from "mockjs";
+import Mock from 'mockjs';
 
 export default {
-  install: () => {
-    Mock.setup({
-      timeout: "600-1200",
-    });
-    const files = import.meta.globEager("./*.js");
-    console.info(files);
-    Object.entries(files).forEach(([key, module]) => {
-      if (key === "./index.js") return;
-      for (let [path, target] of Object.entries(module.default)) {
-        const [rtype, rurl] = path.split("|");
-        const [, controller] = key.split(".");
-        Mock.mock(controller + rurl, rtype, target);
-      }
-    });
-  },
+	install: () => {
+		Mock.setup({
+			timeout: '600-1200',
+		});
+		const files = import.meta.globEager('./*.js');
+		console.info(files);
+		Object.entries(files).forEach(([key, module]) => {
+			if (key === './index.js') return;
+			for (let [path, target] of Object.entries(module.default)) {
+				const [rtype, rurl] = path.split('|');
+				const [, controller] = key.split('.');
+				Mock.mock(RegExp(controller + rurl), rtype, target);
+			}
+		});
+	},
 };
--- src/api/user.js
+++ src/api/user.js
@@ -1,4 +1,5 @@
-import request from "@/utils/request";
+import request from '@/utils/request';
 
-export const userLogin = (data) => request.post("/user/login", data);
-export const getUserInfo = () => request.get("/user/info");
+export const userLogin = (data) => request.post('/user/login', data);
+export const getUserInfo = () => request.get('/user/info');
+export const getUserList = (data) => request.get('/user/list', data);
